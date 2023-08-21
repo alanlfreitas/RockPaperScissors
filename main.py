e326b8d84240231a989c1e100842dc0cb75c2652
alanlfreitas <113162737+alanlfreitas@users.noreply.github.com>
2023-08-21 16:56:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+
+#random_interger = random.randint(1,10)
+#print(random_interger)
+
+#random_float = random.random() * 5
+#print(random_float)
+
+#love_score = random.randint(1,100)
+#print(f"Your love score is {love_score}.")
+
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+#import random
+
+flipcoin = random.randint(1,2)
+if flipcoin == 1:
+    print("Heads")
+else:
+    print("Tails")
